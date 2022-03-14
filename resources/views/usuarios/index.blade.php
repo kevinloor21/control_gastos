da3dd74e05e66a9ce4d3de7ddcb279bf75d8d568
Kevin Loor <kevinloor209@gmail.com>
2022-03-14 11:49:24 -0500

    Primera captura comenzando edit en categorias

---
+++ resources/views/usuarios/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+
+<h1 class="text-white" class="bg-info">𝘓𝘪𝘴𝘵𝘢 𝘥𝘦 𝘶𝘴𝘶𝘢𝘳𝘪𝘰𝘴</h1>
+
+<a href="{{route('usuarios.create')}}" class="btn btn-primary">Nuevo Usuario</a>
+
+<table class="table text-white">
+<th>#</th>
+<th>Nombre</th>
+<th>Cedula</th>
+
+@foreach($users as $u)
+<tr>
+
+<td>{{$loop->iteration}} </td>
+<td>{{$u->usu_name}} </td>
+<td>{{$u->usu_cedula}} </td>
+
+
+</tr>
+@endforeach
+</table>
+@endsection
+
